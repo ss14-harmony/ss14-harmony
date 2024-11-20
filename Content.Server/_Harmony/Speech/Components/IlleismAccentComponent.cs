10b4d97d853fe0d187f4ce18a74f85bfbb6e72b5
Preston Smith <92108534+thetolbean@users.noreply.github.com>
2024-11-20 10:57:56 -0600

    namespace change 2
    Co-authored-by: FluffMe <1780586+FluffMe@users.noreply.github.com>
    Signed-off-by: Preston Smith <92108534+thetolbean@users.noreply.github.com>

--- Content.Server/_Harmony/Speech/Components/IlleismAccentComponent.cs
+++ Content.Server/_Harmony/Speech/Components/IlleismAccentComponent.cs
@@ -1,6 +1,6 @@
 using Content.Server.Speech.EntitySystems;
 
-namespace Content.Server.Speech.Components;
+namespace Content.Server._Harmony.Speech.Components;
 
 [RegisterComponent]
 [Access(typeof(IlleismAccentSystem))]
