e8fa630e3c4bf0d5e8b6836a69b8c31106d10ee5
NakataRin <45946146+NakataRin@users.noreply.github.com>
2025-01-30 18:01:09 +0400

    visuals update

--- Content.Shared/_Harmony/Clothing/Gloves/Systems/SharedMantisGlovesSystem.cs
+++ Content.Shared/_Harmony/Clothing/Gloves/Systems/SharedMantisGlovesSystem.cs
@@ -1,11 +1,14 @@
 using Content.Shared.Item.ItemToggle.Components;
 using Content.Shared._Harmony.Clothing.Gloves.Components;
+using Content.Shared.Clothing.Components;
+using Content.Shared.Clothing.EntitySystems;
 
 namespace Content.Shared._Harmony.Clothing.Gloves.Systems;
 
 public abstract class SharedMantisGlovesSystem : EntitySystem
 {
     [Dependency] private readonly MetaDataSystem _metaData = default!;
+    [Dependency] private readonly ClothingSystem _clothing = default!;
 
     public override void Initialize()
     {
@@ -17,6 +20,11 @@ public abstract class SharedMantisGlovesSystem : EntitySystem
     private void OnToggled(EntityUid uid, MantisGlovesComponent component, ref ItemToggledEvent args)
     {
         UpdateMantisGlovesState(uid, args.Activated, component);
+
+        if (!TryComp<ClothingComponent>(uid, out var clothing))
+            return;
+
+        _clothing.SetEquippedPrefix(uid, args.Activated ? "activated" : null, clothing);
     }
 
     private void UpdateMantisGlovesState(EntityUid uid, bool activated, MantisGlovesComponent? component = null)
