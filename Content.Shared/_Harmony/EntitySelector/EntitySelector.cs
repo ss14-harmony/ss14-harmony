a036b68a167e23dcd303efb49504efac9f030cc2
youtissoum <51883137+youtissoum@users.noreply.github.com>
2025-02-26 22:40:03 +0100

    Basic entity selector support

---
+++ Content.Shared/_Harmony/EntitySelector/EntitySelector.cs
@@ -0,0 +1,51 @@
+﻿using Robust.Shared.Utility;
+
+namespace Content.Shared._Harmony.EntitySelector;
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class EntitySelector
+{
+    [Dependency] protected readonly IEntityManager EntityManager = default!;
+
+    public bool Initialized { get; private set; }
+
+    [DataField]
+    public List<EntitySelector> SubSelectors = new();
+
+    /// <summary>
+    /// One-time initialization of an entity selector.
+    /// Recursively initializes all sub-selectors.
+    /// </summary>
+    [MustCallBase]
+    public virtual void Initialize(IEntitySystemManager entitySystemManager)
+    {
+        DebugTools.Assert(!Initialized, "Tried to initialize an entity selector twice.");
+
+        IoCManager.InjectDependencies(this);
+
+        Initialized = true;
+
+        foreach (var subSelector in SubSelectors)
+        {
+            if (!subSelector.Initialized)
+                subSelector.Initialize(entitySystemManager);
+        }
+    }
+
+    /// <summary>
+    /// Checks if the entity should get selected by the entity selector.
+    /// </summary>
+    [MustCallBase]
+    public virtual bool Matches(EntityUid entity)
+    {
+        DebugTools.Assert(Initialized, "Tried to use an entity selector before initializing it.");
+
+        foreach (var subSelector in SubSelectors)
+        {
+            if (!subSelector.Matches(entity))
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Content.Shared/_Harmony/EntitySelector/Systems/EntitySelectorSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Harmony.EntitySelector.Systems;
+
+public sealed class EntitySelectorSystem : EntitySystem
+{
+    [Robust.Shared.IoC.Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
+
+    /// <summary>
+    /// Ensures that an <see cref="EntitySelector"/> is correctly initialized.
+    /// </summary>
+    /// <remarks>
+    /// This should always be called before using an <see cref="EntitySelector"/>
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void EnsureInitialized(EntitySelector selector)
+    {
+        if (!selector.Initialized)
+            selector.Initialize(_entitySystemManager);
+    }
+
+    /// <summary>
+    /// Get all entities from the enumerator that match the <paramref name="selector"/>
+    /// </summary>
+    public IEnumerable<EntityUid> GetMatchingEntities(IEnumerable<EntityUid> entities, EntitySelector selector)
+    {
+        EnsureInitialized(selector);
+
+        foreach (var entity in entities)
+        {
+            if (selector.Matches(entity))
+                yield return entity;
+        }
+    }
+}
