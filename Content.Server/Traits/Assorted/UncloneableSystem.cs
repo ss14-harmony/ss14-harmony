72334a36f750b2cc62316d25d862448c8fa4e087
Anzuneth <malachigene@gmail.com>
2025-03-11 04:07:43 -0500

    Unrevivable and Uncloneable Split

---
+++ Content.Server/Traits/Assorted/UncloneableSystem.cs
@@ -0,0 +1,21 @@
+using Content.Shared.Cloning.Events;
+using Content.Shared.Traits.Assorted;
+
+namespace Content.Server.Traits.Assorted;
+
+public sealed class UncloneableSystem : EntitySystem
+{
+/// Harmony - New Uncloneable Trait
+    public override void Initialize()
+    {
+        base.Initialize();
+
+       SubscribeLocalEvent<UncloneableComponent, CloningAttemptEvent>(OnCloningAttempt);
+    }
+
+    private void OnCloningAttempt(Entity<UncloneableComponent> ent, ref CloningAttemptEvent args)
+    {
+        if (!ent.Comp.Cloneable)
+            args.Cancelled = true;
+    }
+}
