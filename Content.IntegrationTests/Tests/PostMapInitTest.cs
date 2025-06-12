7134d71514c6e6cb57c352d20ee7ee46c218de3c
Lachryphage <lachri.lachryphage@gmail.com>
2025-06-12 23:49:26 +1000

    Vanquish

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -72,6 +72,7 @@ namespace Content.IntegrationTests.Tests
             "Atlas", // Harmony revived by Kravin
             "Mira", // Harmony, developed by tanuko
             "Eclipse", //Harmony, Developed by Lachryphage
+            "Vanquish", // Harmony, Developed by Lachryphage
             "Prime", // Harmony, developed/revived by luckyshotpictures
             "Spire", // Harmony, Developed by Dogbone10, PenguinCodest, Jack_the_Dragon
             "Dev",
