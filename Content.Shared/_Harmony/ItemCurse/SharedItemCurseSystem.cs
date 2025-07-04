c9822b65e753e4f66367d5ee5a06c0168570bd05
TheSecondLord <88201625+TheSecondLord@users.noreply.github.com>
2025-07-04 11:16:05 +0100

    action refactor

--- Content.Shared/_Harmony/ItemCurse/SharedItemCurseSystem.cs
+++ Content.Shared/_Harmony/ItemCurse/SharedItemCurseSystem.cs
@@ -1,7 +1,9 @@
 using Content.Shared.Actions;
+using Content.Shared.Actions.Components;
 using Content.Shared.Body.Components;
 using Content.Shared.Hands.Components;
 using Content.Shared.Hands.EntitySystems;
+using Content.Shared.ItemRecall;
 using Content.Shared.Popups;
 using Content.Shared.Projectiles;
 using Content.Shared.Throwing;
@@ -82,17 +84,15 @@ public abstract class SharedItemCurseSystem : EntitySystem
         if (!Resolve(ent.Owner, ref ent.Comp, false))
             return;
 
-        if (!TryComp<InstantActionComponent>(ent.Comp.MarkedByAction, out var instantAction))
+        if (_actions.GetAction(ent.Comp.MarkedByAction) is not { } action)
             return;
 
-        var actionOwner = instantAction.AttachedEntity;
-
-        if (actionOwner == null)
+        if (action.Comp.AttachedEntity is not { } user)
             return;
 
         // Un-embed embeddable projectiles
         if (TryComp<EmbeddableProjectileComponent>(ent, out var projectile))
-            _proj.EmbedDetach(ent, projectile, actionOwner.Value);
+            _proj.EmbedDetach(ent, projectile, user);
 
         // Check if the cursed item is in any container.
         // If it is, check each outer container until either finding a body to target or running out of containers.
@@ -127,81 +127,69 @@ public abstract class SharedItemCurseSystem : EntitySystem
 
     private void TryMarkItem(Entity<ItemCurseComponent> ent, EntityUid item)
     {
-        if (!TryComp<InstantActionComponent>(ent, out var instantAction))
+        if (_actions.GetAction(ent.Owner) is not { } action)
             return;
 
-        var actionOwner = instantAction.AttachedEntity;
-
-        if (actionOwner == null)
+        if (action.Comp.AttachedEntity is not { } user)
             return;
 
-        var marker = EnsureComp<CurseMarkerComponent>(item);
         ent.Comp.MarkedEntity = item;
         Dirty(ent);
 
-        marker.MarkedByAction = ent.Owner;
-
-        UpdateActionAppearance(ent);
+        var marker = AddComp<CurseMarkerComponent>(item);
+        marker.MarkedByAction = ent;
         Dirty(item, marker);
+
+        UpdateActionAppearance((action, action, ent));
     }
 
     private void TryUnmarkItem(Entity<CurseMarkerComponent> item)
     {
-        var marker = item.Comp;
-
-        if (!TryComp<InstantActionComponent>(marker.MarkedByAction, out var instantAction))
+        if (!TryComp<CurseMarkerComponent>(item, out var marker))
             return;
 
-        if (TryComp<ItemCurseComponent>(marker.MarkedByAction, out var action))
+        if (_actions.GetAction(marker.MarkedByAction) is not { } action)
+            return;
+
+        if (TryComp<ItemCurseComponent>(action, out var itemCurse))
         {
             // The following comment was from the recall code I copied. I'll just leave it here because it's probably important. TL;DR this code doesn't work yet.
             //
             // For some reason client thinks the station grid owns the action on client and this doesn't work. It doesn't work in PopupEntity(mispredicts) and PopupPredicted either(doesnt show).
             // I don't have the heart to move this code to server because of this small thing.
             // This line will only do something once that is fixed.
-            if (instantAction.AttachedEntity != null)
+            if (action.Comp.AttachedEntity is { } user)
             {
-                _popups.PopupClient(Loc.GetString("item-recall-item-unmark", ("item", item)), instantAction.AttachedEntity.Value, instantAction.AttachedEntity.Value, PopupType.MediumCaution);
+                _popups.PopupClient(Loc.GetString("item-recall-item-unmark", ("item", item)), user, user, PopupType.MediumCaution);
             }
 
-            action.MarkedEntity = null;
-            UpdateActionAppearance((marker.MarkedByAction.Value, action));
-            Dirty(marker.MarkedByAction.Value, action);
+            itemCurse.MarkedEntity = null;
+            UpdateActionAppearance((action, action, itemCurse));
+            Dirty(action, itemCurse);
         }
 
         RemCompDeferred<CurseMarkerComponent>(item);
     }
 
-    private void UpdateActionAppearance(Entity<ItemCurseComponent> action)
+    private void UpdateActionAppearance(Entity<ActionComponent, ItemCurseComponent> action)
     {
-        if (!TryComp<InstantActionComponent>(action, out var instantAction))
-            return;
-
-        if (action.Comp.MarkedEntity == null)
+        if (action.Comp2.MarkedEntity is { } marked)
         {
-            if (action.Comp.InitialName != null)
-                _metaData.SetEntityName(action, action.Comp.InitialName);
-            if (action.Comp.InitialDescription != null)
-                _metaData.SetEntityDescription(action, action.Comp.InitialDescription);
-            _actions.SetEntityIcon(action, null, instantAction);
+            if (action.Comp2.WhileMarkedName is { } name)
+                _metaData.SetEntityName(action, Loc.GetString(name, ("item", marked)));
+
+            if (action.Comp2.WhileMarkedDescription is { } desc)
+                _metaData.SetEntityDescription(action, Loc.GetString(desc, ("item", marked)));
+
+            _actions.SetEntityIcon((action, action), marked);
         }
         else
         {
-            if (action.Comp.WhileMarkedName != null)
-            {
-                _metaData.SetEntityName(action,
-                    Loc.GetString(action.Comp.WhileMarkedName,
-                    ("item", action.Comp.MarkedEntity.Value)));
-            }
-
-            if (action.Comp.WhileMarkedDescription != null)
-            {
-                _metaData.SetEntityDescription(action,
-                    Loc.GetString(action.Comp.WhileMarkedDescription,
-                    ("item", action.Comp.MarkedEntity.Value)));
-            }
-
-            _actions.SetEntityIcon(action, action.Comp.MarkedEntity, instantAction);
+            if (action.Comp2.InitialName is { } name)
+                _metaData.SetEntityName(action, name);
+            if (action.Comp2.InitialDescription is { } desc)
+                _metaData.SetEntityDescription(action, desc);
+            _actions.SetEntityIcon((action, action), null);
         }
     }
 
