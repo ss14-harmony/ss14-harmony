289695a2637ef328e2e8376312fa4952491bcc06
Super <84590915+SuperGDPWYL@users.noreply.github.com>
2025-09-18 13:52:39 +0100

    hopefully fix the test fails, thanks
    Co-authored-by: Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
    Signed-off-by: Super <84590915+SuperGDPWYL@users.noreply.github.com>

--- Content.Shared/_Harmony/Malfunction/MalfunctionEvents.cs
+++ Content.Shared/_Harmony/Malfunction/MalfunctionEvents.cs
@@ -4,69 +4,50 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared._Harmony.Malfunction;
 
-public sealed partial class MalfShopActionEvent : InstantActionEvent
-{
-}
+public sealed partial class MalfShopActionEvent : InstantActionEvent;
 
-public sealed partial class MalfDoomsdayStartEvent : InstantActionEvent
-{
-}
+public sealed partial class MalfDoomsdayStartEvent : InstantActionEvent;
 
-public sealed partial class MalfDestroyRcdsEvent : InstantActionEvent
-{
-}
+public sealed partial class MalfDestroyRcdsEvent : InstantActionEvent;
 
-public sealed partial class MalfDoomsdayActivatedEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfDoomsdayActivatedEvent : EntityEventArgs;
 
-public sealed partial class MalfLockdownEvent : InstantActionEvent
-{
-}
+public sealed partial class MalfLockdownEvent : InstantActionEvent;
 
-public sealed partial class MalfOverloadMachineActionEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfOverloadMachineActionEvent : EntityEventArgs;
 
+[DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class MalfPurchaseOverrideAiaEvent : EntityEventArgs
-{
-}
-[Serializable, NetSerializable]
-public sealed partial class MalfPurchaseOverloadMachineEvent : EntityEventArgs
-{
-}
-[Serializable, NetSerializable]
-public sealed partial class MalfPurchaseVoiceModulationEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfPurchaseOverrideAiaEvent : EntityEventArgs;
 
+[DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class MalfPurchaseDisableControlPanelEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfPurchaseOverloadMachineEvent : EntityEventArgs;
 
+[DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class MalfPurchaseOverloadLightEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfPurchaseVoiceModulationEvent : EntityEventArgs;
 
+[DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class MalfPurchaseJamFirelockEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfPurchaseDisableControlPanelEvent : EntityEventArgs;
 
+[DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class MalfPurchaseOverrideSafetyEvent : EntityEventArgs // this caused a test fail earlier so time to find out if it's a heisentest or not
-{
-}
+public sealed partial class MalfPurchaseOverloadLightEvent : EntityEventArgs;
 
+[DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class MalfPurchaseTurretUpgradeEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfPurchaseJamFirelockEvent : EntityEventArgs;
 
+[DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class MalfOverloadMachineFinishedEvent : EntityEventArgs
-{
-}
+public sealed partial class MalfPurchaseOverrideSafetyEvent : EntityEventArgs;
+
+[DataDefinition]
+[Serializable, NetSerializable]
+public sealed partial class MalfPurchaseTurretUpgradeEvent : EntityEventArgs;
+
+[DataDefinition]
+[Serializable, NetSerializable]
+public sealed partial class MalfOverloadMachineFinishedEvent : EntityEventArgs;
