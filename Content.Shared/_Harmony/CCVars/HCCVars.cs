f5fbc76c3796c286a49d346a3e7aa341ff621a43
taydeo <tay@funkystation.org>
2026-02-24 15:19:38 -0600

    Merge Content warnings from Funky Station

--- Content.Shared/_Harmony/CCVars/HCCVars.cs
+++ Content.Shared/_Harmony/CCVars/HCCVars.cs
@@ -26,4 +26,22 @@ public sealed class HCCVars
     /// </summary>
     public static readonly CVarDef<int> MaxQueuePlayerCount =
         CVarDef.Create("queue.max_player_count", 0, CVar.SERVERONLY); // Client doesn't care about this CVar whatsoever
+
+    /// <summary>
+    /// If the content warning should be displayed.
+    /// </summary>
+    public static readonly CVarDef<bool> ContentWarningDisplay =
+    CVarDef.Create("cw.display", true, CVar.SERVER | CVar.REPLICATED);
+
+    /// <summary>
+    /// If ignoring the content warning should kick you from the server.
+    /// </summary>
+    public static readonly CVarDef<bool> ContentWarningKickOnIgnore =
+        CVarDef.Create("cw.kick", true, CVar.SERVER | CVar.REPLICATED);
+
+    /// <summary>
+    /// If the content warning popup was acknowledged.
+    /// </summary>
+    public static readonly CVarDef<bool> ContentWarningAcknowledged =
+        CVarDef.Create("cw.acknowledged", false, CVar.CLIENTONLY | CVar.ARCHIVE);
 }
