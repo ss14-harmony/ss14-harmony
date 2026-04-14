c1853a72fc3fc78154ffb1cbc0f5e5afd289dccb
UmbraPSV <umbrapsv@protonmail.com>
2026-04-14 13:46:23 +0200

    Fix Dupe RegisterComponent error

--- Content.Server/_Harmony/Speech/Components/RussianAccentHarmonyComponent.cs
+++ Content.Server/_Harmony/Speech/Components/RussianAccentHarmonyComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server.Speech.Components;
 
 [RegisterComponent]
-public sealed partial class RussianAccentComponent : Component
+public sealed partial class RussianAccentHarmonyComponent : Component
 {
     /// <summary>
     /// The chance (0.0 to 1.0) that articles like "the", "a", "an" will be removed from sentences, default is 80%.
