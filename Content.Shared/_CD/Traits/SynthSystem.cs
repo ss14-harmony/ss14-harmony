84f265fb21e5c89d5f9d7378a7ffbe6e02eb6e17
Falcon <falcon@zigtag.dev>
2026-07-11 23:35:02 -0700

    refactor: update for upmerges

--- Content.Shared/_CD/Traits/SynthSystem.cs
+++ Content.Shared/_CD/Traits/SynthSystem.cs
@@ -1,17 +1,19 @@
+using Content.Shared.Body.Components; // Harmony
 using Content.Shared.Body.Systems; // Misfit - Move synthetic trait to shared
 using Content.Shared.Chat.TypingIndicator;
+using Content.Shared.Chemistry.Components; // Harmony
 using Content.Shared.Chemistry.Reagent;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared._CD.Traits; // Misfit - Move synthetic trait to shared
 
-public sealed class SynthSystem : EntitySystem
+public sealed partial class SynthSystem : EntitySystem
 {
     private static readonly ProtoId<TypingIndicatorPrototype> RobotTypingIndicator = "robot"; // Misfit - Type safety
     private static readonly ProtoId<ReagentPrototype> SynthBlood = "SynthBlood"; // Misfit - Type safety
 
-    [Dependency] private readonly SharedBloodstreamSystem _bloodstream = default!; // Misfit - Move synthetic trait to shared
-    [Dependency] private readonly SharedTypingIndicatorSystem _typingIndicator = default!; // Misfit - Partial typing indicator change
+    [Dependency] private SharedBloodstreamSystem _bloodstream = default!; // Misfit - Move synthetic trait to shared
+    [Dependency] private SharedTypingIndicatorSystem _typingIndicator = default!; // Misfit - Partial typing indicator change
 
     public override void Initialize()
     {
@@ -24,7 +26,16 @@ public sealed class SynthSystem : EntitySystem
     {
         _typingIndicator.SetIndicatorPrototype(uid, RobotTypingIndicator); // Misfit - Type safety and partial typing indicator change
 
+        // Harmony Start - Update for new bloodstream system
+        if (!TryComp<BloodstreamComponent>(uid, out var bloodstreamComponent))
+            return;
+
+        var bloodstreamVolume = bloodstreamComponent.BloodReferenceSolution.Volume;
+
+        var synthBloodSolution = new Solution(SynthBlood, bloodstreamVolume);
+        // Harmony End
+
         // Give them synth blood. Ion storm notif is handled in that system
-        _bloodstream.ChangeBloodReagent(uid, SynthBlood); // Misfit - Type safety
+        _bloodstream.ChangeBloodReagents(uid, synthBloodSolution); // Misfit - Type safety // Harmony - Update to ChangeBloodReagents
     }
 }
