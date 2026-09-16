f54e0d49f3d5cb346ca76b2e1af1326c94075ddb
TheFlyingSentry <AFlyingSentry@gmail.com>
2026-09-16 05:15:09 -0400

    Reinforcing the Zombie Horde: Added Zombie self revival

--- Content.Shared/Zombies/ZombieComponent.cs
+++ Content.Shared/Zombies/ZombieComponent.cs
@@ -158,4 +158,19 @@ public sealed partial class ZombieComponent : Component
     /// </summary>
     [DataField("newBloodReagents")]
     public Solution NewBloodReagents = new([new("ZombieBlood", 1)]);
+
+    // Start of Harmony Changes: Reinforcing the Zombie Horde
+
+    /// <summary>
+    /// The percent threshold at which a zombie gets revived if they're dead
+    /// HARMONY ADDITION!!!!!!!!
+    /// </summary>
+    /// <remarks>
+    /// Currently set to revive at 105 HP for humanoids
+    /// Can't set it to a float or butterflies won't revive (very important)
+    /// </remarks>
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float ZombieReviveThreshold = 0.525f;
+
+    // End of Harmony Changes
 }
