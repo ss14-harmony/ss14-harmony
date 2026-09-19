dcb9905f9a4c33ea39d63154f9d76fb006b10042
TheFlyingSentry <AFlyingSentry@gmail.com>
2026-09-19 06:36:43 -0400

    HUUUGE COMMIT HAHAHA *coughs* Revenant Ghostly Whisper

--- Content.Server/Revenant/EntitySystems/RevenantSystem.Abilities.cs
+++ Content.Server/Revenant/EntitySystems/RevenantSystem.Abilities.cs
@@ -31,6 +31,16 @@ using Robust.Shared.Map.Components;
 using Content.Shared.Whitelist;
 using Robust.Shared.Prototypes;
 using Content.Shared.IdentityManagement;
+// Harmony Additions: Revenant Whispering
+using Robust.Shared.Player;
+using Content.Shared._Harmony.Revenant;
+using Content.Shared.Actions;
+using Content.Shared.Chat;
+using Content.Shared.Database;
+using Content.Server.Administration;
+using Content.Server.Administration.Logs;
+using Content.Server.Chat.Managers;
+// End of Harmony Additions
 
 namespace Content.Server.Revenant.EntitySystems;
 
@@ -46,7 +56,11 @@ public sealed partial class RevenantSystem
     [Dependency] private EntityWhitelistSystem _whitelistSystem = default!;
     [Dependency] private SharedTransformSystem _transformSystem = default!;
     [Dependency] private SharedMapSystem _mapSystem = default!;
-
+    // Harmony Additions: Revenant Whispering
+    [Dependency] private QuickDialogSystem _quickDialog = default!;
+    [Dependency] private IChatManager _chatManager = default!;
+    [Dependency] private IAdminLogManager _adminLogger = default!;
+    // End of Harmony Additions
     [Dependency] private EntityQuery<TagComponent> _tagQuery = default!;
     [Dependency] private EntityQuery<ItemComponent> _itemQuery = default!;
     [Dependency] private EntityQuery<PoweredLightComponent> _poweredLightQuery = default!;
@@ -64,6 +78,8 @@ public sealed partial class RevenantSystem
         SubscribeLocalEvent<RevenantComponent, RevenantOverloadLightsActionEvent>(OnOverloadLightsAction);
         SubscribeLocalEvent<RevenantComponent, RevenantBlightActionEvent>(OnBlightAction);
         SubscribeLocalEvent<RevenantComponent, RevenantMalfunctionActionEvent>(OnMalfunctionAction);
+
+        SubscribeLocalEvent<RevenantComponent, RevenantWhisperActionEvent>(OnWhisperAction);
     }
 
     private void OnInteract(EntityUid uid, RevenantComponent component, UserActivateInWorldEvent args)
@@ -357,4 +373,39 @@ public sealed partial class RevenantSystem
             _emagSystem.TryEmagEffect(uid, uid, ent);
         }
     }
+// Start of Harmony Additions: Revenant Whispering
+    private void OnWhisperAction(EntityUid uid, RevenantComponent component, EntityTargetActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (args.Target == args.Performer)
+            return;
+
+        if (!TryComp(args.Performer, out ActorComponent? actorS) || !TryComp(args.Target, out ActorComponent? actorT))
+            return;
+
+        _quickDialog.OpenDialog(actorS.PlayerSession, Loc.GetString("revenant-ghostly-whisper-title", ("target", Identity.Entity(args.Target, EntityManager))), Loc.GetString("prayer-popup-notify-pray-ui-message"), (string message) =>
+        {
+            // Prayer popup still works fine for this
+            // Make sure the player's entity still exist and they are very much still a revenant
+            if (actorS?.PlayerSession != null && HasComp<RevenantComponent>(uid))
+                Whisper(actorT.PlayerSession, actorS.PlayerSession, message);
+        });
+
+        args.Handled = true;
+    }
+
+    private void Whisper(ICommonSession target, ICommonSession sender , string message)
+    {
+        if (target.AttachedEntity == null || sender.AttachedEntity == null)
+            return;
+
+        var wrappedMessage = Loc.GetString("chat-manager-entity-whisper-wrap-message", ("entityName", Identity.Name(sender.AttachedEntity.Value, EntityManager)), ("message", FormattedMessage.EscapeText(message)));
+
+        _popup.PopupEntity(Loc.GetString("revenant-ghostly-whisper-popup"), target.AttachedEntity.Value, target, PopupType.Medium);
+        _chatManager.ChatMessageToOne(ChatChannel.Whisper, message, wrappedMessage, target.AttachedEntity.Value, false, target.Channel); // better to show a message to the player in question than make it text chat only
+        _adminLogger.Add(LogType.AdminMessage, LogImpact.Low, $"{ToPrettyString(target.AttachedEntity.Value):player} received a ghostly whisper  from {sender?.Name ?? "unknown sender"}: {message}"); // Logging as Admin since it IS a Subtle Message
+    }
+    // End of Harmony Additions: Revenant Whispering
 }
