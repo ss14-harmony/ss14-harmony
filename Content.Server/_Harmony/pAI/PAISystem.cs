ebacec85cbd1a84306fba11a9eb5e8a8d224ffdb
TheFlyingSentry <AFlyingSentry@gmail.com>
2026-09-23 09:41:00 -0400

    Move some store stuff to its own File

---
+++ Content.Server/_Harmony/pAI/PAISystem.cs
@@ -0,0 +1,28 @@
+﻿using Content.Shared.PAI;
+using Content.Shared.Radio;
+using Content.Shared.Radio.Components;
+
+namespace Content.Server.pAI;
+
+public sealed class PAISystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PAIComponent, EncryptionChannelsChangedEvent>(OnKeysChanged); // Harmony Additions: Added more entries into the pAI store
+    }
+
+    private void OnKeysChanged(EntityUid uid, PAIComponent component, EncryptionChannelsChangedEvent args)
+    {
+        UpdateRadioChannels(uid, component, args.Component);
+    }
+    private void UpdateRadioChannels(EntityUid uid, PAIComponent innate, EncryptionKeyHolderComponent keyHolder)
+    {
+        foreach (var channel in innate.Channels)
+        {
+            keyHolder.Channels.Add(channel);
+        }
+        EnsureComp<ActiveRadioComponent>(uid).Channels = keyHolder.Channels;
+    }
+}
